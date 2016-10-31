7ee4a9fe95e6fb3d805273970f3cd89c62bfc3c4
Hai Tran <hai.tran@infonam.com>
2016-10-31 13:50:04 +0700

    update fix bug add libs

--- app/src/main/java/com/han/ngoc/test_app/MainActivity.java
+++ app/src/main/java/com/han/ngoc/test_app/MainActivity.java
@@ -16,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
         List<Tense_E> list = getData();
 //       ối
     }
-    private List<Tense_E> getData(){
+
+    private List<Tense_E> getData() {
         List<Tense_E> list = new ArrayList<Tense_E>();
-        Tense_E tense_e = new Tense_E("dongnhi","Thì Hiện tại đơn");
-        Tense_E tense_e1 = new Tense_E("khoimy","Thì Qúa khứ đơn");
-        Tense_E tense_e2 = new Tense_E("issac","Thì Hiện tài khoàn thành");
-        Tense_E tense_e3 = new Tense_E("mytam","Thì Hiện tại tiếp diễn");
+        Tense_E tense_e = new Tense_E("dongnhi", "Thì Hiện tại đơn");
+        Tense_E tense_e1 = new Tense_E("khoimy", "Thì Qúa khứ đơn");
+        Tense_E tense_e2 = new Tense_E("issac", "Thì Hiện tài khoàn thành");
+        Tense_E tense_e3 = new Tense_E("mytam", "Thì Hiện tại tiếp diễn");
         list.add(tense_e);
         list.add(tense_e1);
         list.add(tense_e2);
